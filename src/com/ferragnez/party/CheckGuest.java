db3d32ba62d421ca8c8007c5833f0a8ffbe5940e
Giacomotestori <giacomo.testori10@gmail.com>
2023-07-13 14:37:42 +0200

    for cicle checked guest name

--- src/com/ferragnez/party/CheckGuest.java
+++ src/com/ferragnez/party/CheckGuest.java
@@ -11,7 +11,27 @@ public class CheckGuest {
         String name = scanner.nextLine();
 
 
-        
+        //verificare se il nome è presente nella lista
+
+        boolean guestpresent = false;
+
+        for(int i=0; i< GuestList.length; i++){
+            if(GuestList[i].equals(name)){
+                guestpresent = true;
+            }
+        }
+
+        //stampare se il nome è presente:"prego può entrare" e se non è presente stampare "mi spiace non può entrare"
+
+        if(guestpresent){
+            System.out.println("Prego può entrare");
+        } else{
+            System.out.println("Mi spiace non può entrare");
+        }
+
+
+
+
 
         scanner.close();
 
