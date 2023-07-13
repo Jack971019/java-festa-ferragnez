62567ac6be4e71a45a37d387e339b4d10426eb17
Giacomotestori <giacomo.testori10@gmail.com>
2023-07-13 14:26:43 +0200

    asked guest name

--- src/com/ferragnez/party/CheckGuest.java
+++ src/com/ferragnez/party/CheckGuest.java
@@ -1,7 +1,19 @@
 package com.ferragnez.party;
+import java.util.Scanner;
 
 public class CheckGuest {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String[] GuestList = {"Dua Lipa", "Paris Hilton", "Manuel Agnelli", "J-Ax", "Francesco Totti", "Ilary Blasi", "Bebe Vio", "Luis", "Pardis Zarei", "Martina Maccherone", "Rachel Zeilic"};
+
+        System.out.println("inserisci il tuo nome:");
+        String name = scanner.nextLine();
+
+
+        
+
+        scanner.close();
 
     }
 }
